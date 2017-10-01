dc28a43d4fdf39891ceb7255d2e9dc044673ff6e
JCharante <git@jcharante.com>
2017-10-01 17:51:01 -0400

    add GitHub info to Contact page

--- landing/src/components/Contact.vue
+++ landing/src/components/Contact.vue
@@ -13,8 +13,14 @@
         <div class="layout-padding">
             <p class="strong">Email</p>
             <p>You can always email me at contactme@jcharante.com</p>
+
+            <p class="strong">GitHub</p>
+            <p>I'm <a href="https://github.com/JCharante">JCharante</a> on Github.</p>
+            <gh-btns-follow user="JCharante" show-count></gh-btns-follow> <p style="visibility: hidden">a</p>
+
             <p class="strong">WeChat</p>
             <p>Feel free to add me on WeChat (@JCharante).</p>
+
             <p class="strong">Mail</p>
             <p>If you want to send me some foreign vegan candy, you can do so to </p>
             <blockquote>
