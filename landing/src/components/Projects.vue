ce5daf64a3b64d260de015b69de5a6ef50b6ea20
JCharante <git@jcharante.com>
2017-10-02 16:03:14 -0400

    add info to Projects page

--- landing/src/components/Projects.vue
+++ landing/src/components/Projects.vue
@@ -11,9 +11,22 @@
         <drawer slot="left"/>
 
         <div class="layout-padding">
-            <p>I'll add something here eventually.</p>
             <p>
-                For now you can just look at <a href="https://github.com/JCharante">my Github profile</a>
+                <a href="https://github.com/JCharante/haulers-channel-reward-calc">
+                    <strong>JCharante/haulers-channel-reward-calc</strong>
+                </a>
+            </p>
+            <p>
+                An interstellar shipping expense calculator for an online community commissioned by a regional trading
+                mogul and operator of said community.
+            </p>
+            <p>
+                <router-link to="blog/haulers-channel-reward-calc">View the writeup here</router-link>
+            </p>
+            <hr>
+            <p>
+                I'm still working on adding content, so for now you can just look at
+                <a href="https://github.com/JCharante">my Github profile</a>.
             </p>
         </div>
     </q-layout>
