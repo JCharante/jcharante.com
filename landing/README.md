7945af962a2fbb3a3f92b7ddd531f1a92d34b738
JCharante <git@jcharante.com>
2017-10-04 19:51:46 -0400

    add prismjs

--- landing/README.md
+++ landing/README.md
@@ -17,3 +17,12 @@ $ quasar build
 # lint code
 $ quasar lint
 ```
+
+
+## Prism
+
+This is a bit weird, but you need to modify vue-prism
+so instead of assigning the token class, it assigns the
+tokeno class. And modify the css file, to affect the tokeno
+class instead of the token class. This is because of a conflict
+with Quasar.
--- landing/src/main.js
+++ landing/src/main.js
@@ -15,10 +15,13 @@ import Quasar from 'quasar'
 import router from './router'
 import VueGitHubButtons from 'vue-github-buttons'
 import 'vue-github-buttons/dist/vue-github-buttons.css'
+import VuePrism from 'vue-prism'
+import 'prismjs/themes/prism.css'
 
 Vue.config.productionTip = false
 Vue.use(VueGitHubButtons)
 Vue.use(Quasar) // Install Quasar Framework
+Vue.use(VuePrism)
 
 if (__THEME === 'mat') {
     require('quasar-extras/roboto-font')
