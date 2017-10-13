19e02aeacf3790ee04812bf0a0a1c0d07936ca0f
JCharante <git@jcharante.com>
2017-10-13 19:39:46 -0400

    add styling to Contact page

--- landing/src/components/Contact.vue
+++ landing/src/components/Contact.vue
@@ -10,7 +10,7 @@
 
         <drawer slot="left"/>
 
-        <div class="layout-padding">
+        <div class="layout-padding" style="max-width: 90vw; width: 800px;">
             <p class="strong">Email</p>
             <p>You can always email me at contactme@jcharante.com</p>
 
