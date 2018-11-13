e0a3d043f22562fab223fa794b5c451b5eb1364f
jcharante <git@jcharante.com>
2018-11-13 11:20:03 -0500

    update footer

--- redesign/src/components/Footer.vue
+++ redesign/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row justify-between">
-    <p>Powered by <a href="https://quasar-framework.org">Quasar Framework</a></p>
-    <p>With thanks to <a href="http://www.aaronsw.com/">Aaron Swartz</a>.</p>
+    <p>Powered by <a href="https://quasar-framework.org">Quasar Framework</a> and hosted on <a href="https://now.sh">now.sh</a></p>
+    <p>In loving memory of <a href="http://www.aaronsw.com/">Aaron Swartz</a>.</p>
   </div>
 </template>
