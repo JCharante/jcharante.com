5204c1dcef8006c601cda2fb2e231ab182c3567a
jcharante <git@jcharante.com>
2019-01-10 08:43:19 -0500

    fix

--- redesign/src/pages/Thoughts/190110.vue
+++ redesign/src/pages/Thoughts/190110.vue
@@ -7,7 +7,7 @@
       <p>Saluton! Mi volis skribi <q-chip square>hieraux</q-chip> sed mi ne <q-chip square>finu</q-chip> mian <q-chip square>hejmtasko gxis</q-chip> hodiaux. (Mi ne povas <q-chip square>kredi</q-chip> ke mi <q-chip square>forgesis</q-chip> gxis...)</p>
       <p>Hodiaux mi nur havas du horaj de klasoj, do mi sxatas <q-chip square>tion!</q-chip></p>
       <p><q-chip square>Morgaux</q-chip> mi havas mia claso de Operating Systems. Mi nur havas tion klaso <q-chip square>du fojojn por semajno.</q-chip></p>
-      <p>Mi pensas ke en la futuro mi volas <q-chip square>krei</q-chip> <q-chip square>filmetoj</q-chip> de mian tagoj, sed ne cxi tion monato... aux la venonta.. zzzz</p>
+      <p>Mi pensas ke en la futuro mi volas <q-chip square>krei</q-chip> <q-chip square>filmetoj</q-chip> de mian tagoj, sed ne cxi tion monato... aux la <q-chip square>venonta..</q-chip> zzzz</p>
     </div>
   </q-page>
 </template>
