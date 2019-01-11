2480751f90112466f9844744953b6c62369591ac
jcharante <git@jcharante.com>
2019-01-11 00:18:48 -0500

    update

--- redesign/src/pages/Thoughts/190110.vue
+++ redesign/src/pages/Thoughts/190110.vue
@@ -8,6 +8,12 @@
       <p>Hodiaux mi nur havas du horaj de klasoj, do mi sxatas <q-chip square>tion!</q-chip></p>
       <p><q-chip square>Morgaux</q-chip> mi havas mia claso de Operating Systems. Mi nur havas tion klaso <q-chip square>du fojojn por semajno.</q-chip></p>
       <p>Mi pensas ke en la futuro mi volas <q-chip square>krei</q-chip> <q-chip square>filmetoj</q-chip> de mian tagoj, sed ne cxi tion monato... aux la <q-chip square>venonta..</q-chip> zzzz</p>
+      <hr>
+      <p>Mi estas bone.. mi estas tre bone.. sed mi ne scias <q-chip square>kial</q-chip> mi ne <q-chip square>sentas</q-chip> bone.</p>
+      <hr>
+      <p>Do gxi estas 11 <q-chip square>nokte</q-chip>, kaj mi sentas tre bone.</p>
+      <p>Mi volas skribi pri robotiko. Ne nur de robotiko, sed de la kulturo de robotiko en reddit.</p>
+      <p>Mi comprendas ke vi devas <q-chip square>amuzas</q-chip> kun robotiko, sed mi tre malsxatas la kulturo de <q-chip square>pigruloj</q-chip>, sed mi devas dormi.</p>
     </div>
   </q-page>
 </template>
