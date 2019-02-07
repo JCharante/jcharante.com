b9c42c7d3bc765ec9077464170466c0b0e9684ad
jcharante <git@jcharante.com>
2019-02-06 23:04:00 -0500

    add to blog

--- redesign/src/pages/Thoughts/190206.vue
+++ redesign/src/pages/Thoughts/190206.vue
@@ -5,13 +5,23 @@
       <div class="q-subheading">190206</div>
       <br>
       <p>Bonan matenon! La alia tago mi finita mia Esperanta arbo en Duolingo (por la dua fojo)</p>
-      <q-btn color="purple" @click="showEsperanto = !showEsperanto" :label="showEsperanto ? 'Kasxi la foto' : 'Elmontri la foto'" />
+      <div class="row">
+        <q-btn color="purple" @click="showEsperanto = !showEsperanto" :label="showEsperanto ? 'Kasxi la foto' : 'Elmontri la foto'" />
+      </div>
       <img v-if="showEsperanto" src="https://static.jcharante.com/blog/esperanto-tree-190206.png">
       <div class="spacer"></div>
       <p>Do vi povas vidi ke mi ne havas nivelo kvin (5) en la tuta kurso/arbo. Estas sesdek du (62) "kapabloj" aux "kategorioj", kaj mia tuta nivelo estas 113. Do mi estas (113/310)% = 36.45% finita kun la kurso.</p>
       <p>Mi volas fini la kurso cxi-monato, do mi devus gajni cent nauxdek sep (197) niveloj en dudek tri (23) tagoj. Do 8.5 niveloj per tago. </p>
       <hr>
       <p>Nun mi devas kolekti mian pakadon el la cxambro de posxto.</p>
+      <hr>
+      <div class="row">
+        <q-btn color="purple" @click="showEsperanto1 = !showEsperanto1" :label="showEsperanto1 ? 'Kasxi la foto' : 'Elmontri la foto'" />
+      </div>
+      <img v-if="showEsperanto1" src="https://static.jcharante.com/blog/esperanto-tree-190206-desktop.png">
+      <div class="spacer"></div>
+      <p>Mi estas finita por hodiaux! (Mi gajnis 9 niveloj hodiaux, do 39.4% finita kun la arbo)</p>
+      <p>(nur kun Duolingo.. mi havas multe de hejmtasko kaj laboro ke mi devas fari...)</p>
     </div>
   </q-page>
 </template>
@@ -21,7 +31,8 @@ export default {
   name: '190206',
   data: function () {
     return {
-      showEsperanto: true
+      showEsperanto: true,
+      showEsperanto1: true
     }
   }
 }
