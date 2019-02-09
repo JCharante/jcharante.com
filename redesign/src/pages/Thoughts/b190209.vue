8d8df844374e9ca8f764b494a45399b4a1105f03
jcharante <git@jcharante.com>
2019-02-09 11:06:37 -0500

    add content

--- redesign/src/pages/Thoughts/b190209.vue
+++ redesign/src/pages/Thoughts/b190209.vue
@@ -3,6 +3,8 @@
     <div class="layout-padding" style="max-width: 90vw; width: 800px;">
       <div class="q-title">Mia tago</div>
       <div class="q-subheading">190209</div>
+      <p>Hodiaŭ mi devus legi dokumenton por la ekfirmao de mia amiko.</p>
+      <p>Ankaux, mi rekontigxos kun mia kunkompustisto el mia klaso. Kaj mi laboros pri projekto por mia laboratorio, do hodaiŭ mi estas tre okupa!</p>
     </div>
   </q-page>
 </template>
