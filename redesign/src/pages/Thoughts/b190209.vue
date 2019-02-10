d18398671572b1d9abf105b0e4344960cda99559
jcharante <git@jcharante.com>
2019-02-10 00:22:00 -0500

    add stuff from saturday

--- redesign/src/pages/Thoughts/b190209.vue
+++ redesign/src/pages/Thoughts/b190209.vue
@@ -5,6 +5,13 @@
       <div class="q-subheading">190209</div>
       <p>Hodiaŭ mi devus legi dokumenton por la ekfirmao de mia amiko.</p>
       <p>Ankaux, mi rekontigxos kun mia kunkompustisto el mia klaso. Kaj mi laboros pri projekto por mia laboratorio, do hodaiŭ mi estas tre okupa!</p>
+      <hr>
+      <p>Do.. hodiaux ne estis tre bona.. Mi laboris, sed ne kiom mi volis.</p>
+      <p>En bonnovajxo, nun mi estas (150/310)% aux 48.4% finita kun la kurso!</p>
+      <div class="row">
+        <q-btn color="purple" @click="showEsperanto = !showEsperanto" :label="showEsperanto ? 'Kasxi la foto' : 'Elmontri la foto'" />
+      </div>
+      <img v-if="showEsperanto" src="https://static.jcharante.com/blog/esperanto-tree-190209.png">
     </div>
   </q-page>
 </template>
