5396936062a29508f4720c7337f3c005d83d6c75
jcharante <git@jcharante.com>
2019-02-11 16:09:26 -0500

    add entry for this evening

--- redesign/src/pages/Thoughts/b190211.vue
+++ redesign/src/pages/Thoughts/b190211.vue
@@ -8,6 +8,8 @@
       <p>Hieraux mi gajni ok niveloj, kaj mi estas 51% finita kun la kurso. (<a href="https://static.jcharante.com/blog/esperanto-tree-190210-desktop.png">Miru bildo</a>)</p>
       <p>Mi ne gajni naux nivelojn cxar mi dormis kiam mi studis!</p>
       <p>Nun mi laboras gxis vespermangxo!</p>
+      <hr>
+      <p>Do mi havis rekontigxon je 6:30 en la mateno, kaj nun mi havas renkontigxojn cxi tio vesperon je 4:30, 6:00, kaj 7:00! Miru al mi, havas multajn rekontigxojn kaj fekojn (aux ajxojn se vi ne parolas la anglan).</p>
     </div>
   </q-page>
 </template>
